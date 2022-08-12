3853d62170ec9c0e865ae98abbcb38e4e58d2134
AliNajafi1998 <najafi1998ali@gmail.com>
2022-08-12 13:04:04 +0300

    comment collector is done

--- CommentCollector.py
+++ CommentCollector.py
@@ -34,8 +34,50 @@ def get_comments(video_id, max_page):
 
 
 if __name__ == "__main__":
-    vi_id = "wk63ZY_GVzA"
-    print(get_comments(vi_id, 5))
+    # importing the modules
+    import logging
+    logging.basicConfig(filename='comments.log', filemode='w', format='%(name)s - %(levelname)s - %(message)s')
+    from tqdm import tqdm
+
+    from pathlib import Path    
+    import pandas as pd
+
+    # setting the paths
+    file_path = "file_path"
+    error_file_path = 'error_file_path'
+
+
+    # reading the data
+    df = pd.read_csv(file_path, sep="\t")
+
+    # getting the comments
+    comment_id = {"comment": [], "video_id": []}
+    req_c = 0
+    failed = 0
+    for video_id in tqdm(df['id.videoId'].to_list(),leave=True):
+        req_c += 1 
+        try:
+            comments = get_comments(video_id, 5)  # 5 requests per video id, 100 comments per request
+            for comment in comments:
+                comment_id['comment'].append(comment)
+                comment_id['video_id'].append(video_id)
+        except Exception as e:
+            print(video_id, e)
+            logging.error(msg=str(video_id) + " : " + str(e)+'\n')
+            failed += 1
+            with open(error_file_path,'a')as ef:
+                ef.write(f"{video_id}\n")
+
+    print(file_path)
+    print("Number of requests:",req_c)
+    print("Number of failed requests:",failed)       
+
+    id_comment_df = pd.DataFrame(comment_id)
+    output_file_path = f'\\{Path(file_path).name[:-4]}_comments.tsv'
+    id_comment_df.to_csv(output_file_path, sep='\t')
+
+
+
 
 
 
--- README.md
+++ README.md
@@ -2,6 +2,6 @@
 
 To Do List:
 
-- [ ] collect the comments of youtube videos
+- [x] collect the comments of youtube videos
 - [ ] preprocessing the comments
 - [ ] apply transformers based model to get the sentiments  
