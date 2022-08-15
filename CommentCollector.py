11fe72e4600693a8ef004fa5508690bb984debe2
AliNajafi1998 <najafi1998ali@gmail.com>
2022-08-15 18:55:59 +0300

    clean up

--- CommentCollector.py
+++ CommentCollector.py
@@ -12,13 +12,13 @@ def get_comments(video_id, max_page):
         maxResults=100
     ).execute()
 
-    comments = []
+    collected_comments = []
     # iterate video response
     while video_response:
         for item in video_response['items']:
             # Extracting comments
-            comment = item['snippet']['topLevelComment']['snippet']['textDisplay']
-            comments.append(comment)
+            comment_text = item['snippet']['topLevelComment']['snippet']['textDisplay']
+            collected_comments.append(comment_text)
         if counter < max_page:
             if 'nextPageToken' in video_response:
                 video_response = youtube.commentThreads().list(
@@ -30,22 +30,22 @@ def get_comments(video_id, max_page):
                 break
         else:
             break
-    return comments
+    return collected_comments
 
 
 if __name__ == "__main__":
     # importing the modules
     import logging
+
     logging.basicConfig(filename='comments.log', filemode='w', format='%(name)s - %(levelname)s - %(message)s')
     from tqdm import tqdm
 
-    from pathlib import Path    
+    from pathlib import Path
     import pandas as pd
 
     # setting the paths
     file_path = "file_path"
     error_file_path = 'error_file_path'
-
 
     # reading the data
     df = pd.read_csv(file_path, sep="\t")
@@ -54,31 +54,24 @@ if __name__ == "__main__":
     comment_id = {"comment": [], "video_id": []}
     req_c = 0
     failed = 0
-    for video_id in tqdm(df['id.videoId'].to_list(),leave=True):
-        req_c += 1 
+    for vid_id in tqdm(df['id.videoId'].to_list(), leave=True):
+        req_c += 1
         try:
-            comments = get_comments(video_id, 5)  # 5 requests per video id, 100 comments per request
+            comments = get_comments(vid_id, 5)  # 5 requests per video id, 100 comments per request
             for comment in comments:
                 comment_id['comment'].append(comment)
-                comment_id['video_id'].append(video_id)
+                comment_id['video_id'].append(vid_id)
         except Exception as e:
-            print(video_id, e)
-            logging.error(msg=str(video_id) + " : " + str(e)+'\n')
+            print(vid_id, e)
+            logging.error(msg=str(vid_id) + " : " + str(e) + '\n')
             failed += 1
-            with open(error_file_path,'a')as ef:
-                ef.write(f"{video_id}\n")
+            with open(error_file_path, 'a') as ef:
+                ef.write(f"{vid_id}\n")
 
     print(file_path)
-    print("Number of requests:",req_c)
-    print("Number of failed requests:",failed)       
+    print("Number of requests:", req_c)
+    print("Number of failed requests:", failed)
 
     id_comment_df = pd.DataFrame(comment_id)
     output_file_path = f'\\{Path(file_path).name[:-4]}_comments.tsv'
     id_comment_df.to_csv(output_file_path, sep='\t')
-
-
-
-
-
-
-
