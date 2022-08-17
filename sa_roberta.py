cca16bca936df71a6f67d75df782a3ee1f84c0ef
AliNajafi1998 <najafi1998ali@gmail.com>
2022-08-17 18:04:40 +0300

    attention mask inputs

--- sa_roberta.py
+++ sa_roberta.py
@@ -1,4 +1,4 @@
-# https://huggingface.co/cardiffnlp/twitter-roberta-base-sentiment
+# https://huggingface.co/cardiffnlp/twitter-roberta-base-sentiment-latest
 
 import pandas as pd
 import os
@@ -10,8 +10,7 @@ import torch
 
 # loading the tokenizer and model
 task = 'sentiment'
-MODEL = f"cardiffnlp/twitter-roberta-base-{task}"
-mapping_link = f"https://raw.githubusercontent.com/cardiffnlp/tweeteval/main/datasets/{task}/mapping.txt"
+MODEL = f"cardiffnlp/twitter-roberta-base-{task}-latest"
 
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 tokenizer = AutoTokenizer.from_pretrained(MODEL)
@@ -22,36 +21,11 @@ START_TOKEN_ID = 0
 PAD_TOKEN_ID = 1
 END_TOKEN_ID = 2
 
-
-def get_chunks(token_ids, window_size=510, stride=211):
-    total_length = len(token_ids)
-    flag = True
-    text_chunks = []
-    start = 0
-    while flag:
-        end = start + window_size
-        if end >= total_length:
-            flag = False
-            end = total_length
-
-        # start token
-        input_chunk_ids = torch.concat([torch.IntTensor([START_TOKEN_ID]), token_ids[start:end]])
-
-        # end token
-        input_chunk_ids = torch.concat([input_chunk_ids, torch.IntTensor([END_TOKEN_ID])])
-
-        # padding
-        padding_size = abs(window_size + 2 - len(input_chunk_ids))
-        input_chunk_ids = torch.concat(
-            [input_chunk_ids, torch.IntTensor([PAD_TOKEN_ID] * padding_size)])
-        input_chunk_ids = torch.reshape(input_chunk_ids, [-1, 512])
-        text_chunks.append(input_chunk_ids.to(device))
-        start = start + stride
-    return text_chunks
-
+WINDOW_SIZE = 510
+STRIDE = 211
 
 for root, dirs, files in os.walk(os.getcwd() + "\\data", topdown=False):
-    for file_name in tqdm(files):
+    for file_name in files:
 
         if file_name.endswith("_cleaned.tsv"):
 
@@ -61,28 +35,62 @@ for root, dirs, files in os.walk(os.getcwd() + "\\data", topdown=False):
             df.reset_index(drop=True, inplace=True)
             comments = df['clean_comments'].tolist()
             predictions = []
-            for c in comments:
+            for c in tqdm(comments):
                 # encoding the texts
-                input_ids = tokenizer.encode(c, return_tensors='pt', add_special_tokens=False)[0]
+                tokens = tokenizer.encode_plus(c, return_tensors='pt', add_special_tokens=False)
+
+                input_ids = tokens['input_ids'][0]
+                attention_mask = tokens['attention_mask'][0]
 
                 # windowing
-                chunks = get_chunks(input_ids)
+                total_length = len(input_ids)
+                flag = True
+                text_chunks = []
+                start = 0
+                chunk_scores = []
+                while flag:
+                    end = start + WINDOW_SIZE
+                    if end >= total_length:
+                        flag = False
+                        end = total_length
 
-                # preparing batch
-                chunks = torch.concat(chunks, axis=0).to(device)
+                    # start token
+                    input_chunk_ids = torch.concat([torch.IntTensor([START_TOKEN_ID]), input_ids[start:end]])
+                    attention_chunk_mask = torch.concat([torch.IntTensor([START_TOKEN_ID]), attention_mask[start:end]])
 
-                # batch predicting
-                output = model(chunks)[0].detach()
+                    # end token
+                    input_chunk_ids = torch.concat([input_chunk_ids, torch.IntTensor([END_TOKEN_ID])])
+                    attention_chunk_mask = torch.concat([attention_chunk_mask, torch.IntTensor([END_TOKEN_ID])])
 
-                # softmax scores
-                batch_scores = torch.softmax(output[0], dim=-1)
-                batch_scores = torch.reshape(batch_scores, [-1, 3])
+                    # padding
+                    padding_size = abs(WINDOW_SIZE + 2 - len(input_chunk_ids))
+                    input_chunk_ids = torch.concat([input_chunk_ids, torch.IntTensor([PAD_TOKEN_ID] * padding_size)])
+
+                    attention_chunk_mask = torch.concat(
+                        [attention_chunk_mask, torch.IntTensor([PAD_TOKEN_ID] * padding_size)])
+
+                    input_chunk_ids = torch.reshape(input_chunk_ids, [-1, 512])
+                    attention_chunk_mask = torch.reshape(attention_chunk_mask, [-1, 512])
+
+                    input_dict = {
+                        'input_ids': input_chunk_ids.long().to(device),
+                        'attention_mask': attention_chunk_mask.int().to(device)
+                    }
+
+                    output = model(**input_dict)[0][0].detach()
+                    scores = torch.softmax(output, dim=-1)
+                    chunk_scores.append(scores)
+
+                    start = start + STRIDE
+
+                chunk_scores = torch.stack(chunk_scores)
 
                 # mean probabilities
-                scores = torch.mean(batch_scores, dim=0)
+                final_scores = torch.stack(chunk_scores).mean(dim=0)
 
                 # Labels: 0 -> Negative; 1 -> Neutral; 2 -> Positive
-                pred = torch.argmax(scores).item()
+                pred = torch.argmax(final_scores).item()
+
                 predictions.append(pred)
 
             df["predicted_label"] = predictions
