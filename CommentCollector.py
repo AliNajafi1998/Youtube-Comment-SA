647589d4d97cbddb863ee251970609af57a2cbb0
AliNajafi1998 <najafi1998ali@gmail.com>
2022-08-31 11:46:19 +0300

    refactoring

--- CommentCollector.py
+++ CommentCollector.py
@@ -1,30 +1,31 @@
 from googleapiclient.discovery import build
 import APIKeys
 
-youtube = build('youtube', 'v3', developerKey=APIKeys.API_KEYS[1])
+youtube = build("youtube", "v3", developerKey=APIKeys.API_KEYS[1])
 
 
 def get_comments(video_id, max_page):
     counter = 1
-    video_response = youtube.commentThreads().list(
-        part='snippet,replies',
-        videoId=video_id,
-        maxResults=100
-    ).execute()
+    video_response = (
+        youtube.commentThreads()
+        .list(part="snippet,replies", videoId=video_id, maxResults=100)
+        .execute()
+    )
 
     collected_comments = []
     # iterate video response
     while video_response:
-        for item in video_response['items']:
+        for item in video_response["items"]:
             # Extracting comments
-            comment_text = item['snippet']['topLevelComment']['snippet']['textDisplay']
+            comment_text = item["snippet"]["topLevelComment"]["snippet"]["textDisplay"]
             collected_comments.append(comment_text)
         if counter < max_page:
-            if 'nextPageToken' in video_response:
-                video_response = youtube.commentThreads().list(
-                    part='snippet,replies',
-                    videoId=video_id
-                ).execute()
+            if "nextPageToken" in video_response:
+                video_response = (
+                    youtube.commentThreads()
+                    .list(part="snippet,replies", videoId=video_id)
+                    .execute()
+                )
                 counter += 1
             else:
                 break
@@ -37,7 +38,11 @@ if __name__ == "__main__":
     # importing the modules
     import logging
 
-    logging.basicConfig(filename='comments.log', filemode='w', format='%(name)s - %(levelname)s - %(message)s')
+    logging.basicConfig(
+        filename="comments.log",
+        filemode="w",
+        format="%(name)s - %(levelname)s - %(message)s",
+    )
     from tqdm import tqdm
 
     from pathlib import Path
@@ -45,7 +50,7 @@ if __name__ == "__main__":
 
     # setting the paths
     file_path = "file_path"
-    error_file_path = 'error_file_path'
+    error_file_path = "error_file_path"
 
     # reading the data
     df = pd.read_csv(file_path, sep="\t")
@@ -54,18 +59,20 @@ if __name__ == "__main__":
     comment_id = {"comment": [], "video_id": []}
     req_c = 0
     failed = 0
-    for vid_id in tqdm(df['id.videoId'].to_list(), leave=True):
+    for vid_id in tqdm(df["id.videoId"].to_list(), leave=True):
         req_c += 1
         try:
-            comments = get_comments(vid_id, 5)  # 5 requests per video id, 100 comments per request
+            comments = get_comments(
+                vid_id, 5
+            )  # 5 requests per video id, 100 comments per request
             for comment in comments:
-                comment_id['comment'].append(comment)
-                comment_id['video_id'].append(vid_id)
+                comment_id["comment"].append(comment)
+                comment_id["video_id"].append(vid_id)
         except Exception as e:
             print(vid_id, e)
-            logging.error(msg=str(vid_id) + " : " + str(e) + '\n')
+            logging.error(msg=str(vid_id) + " : " + str(e) + "\n")
             failed += 1
-            with open(error_file_path, 'a') as ef:
+            with open(error_file_path, "a") as ef:
                 ef.write(f"{vid_id}\n")
 
     print(file_path)
@@ -73,5 +80,5 @@ if __name__ == "__main__":
     print("Number of failed requests:", failed)
 
     id_comment_df = pd.DataFrame(comment_id)
-    output_file_path = f'\\{Path(file_path).name[:-4]}_comments.tsv'
-    id_comment_df.to_csv(output_file_path, sep='\t')
+    output_file_path = f"\\{Path(file_path).name[:-4]}_comments.tsv"
+    id_comment_df.to_csv(output_file_path, sep="\t")
